e77ca73a5bfb4d871023aa2819ac55f70d68468e
mateusjunges <mateus@junges.dev>
2022-02-02 00:41:35 -0300

    add v1.6.x branch to laravel kafka docs

--- config/docs.php
+++ config/docs.php
@@ -9,6 +9,7 @@ return [
                 "v1.3.x" => "v1.3",
                 "v1.4.x" => "v1.4",
                 "v1.5.x" => "v1.5",
+                "v1.6.x" => "v1.6",
                 "master" => "v2"
             ],
             "category" => "Laravel"
