fb6c180b3b276fdbb7c4772b3edb6da0ec8831ce
mateusjunges <mateus@junges.dev>
2022-03-23 10:00:08 -0300

    generate docs for laravel-kafka v1.7.x

--- config/docs.php
+++ config/docs.php
@@ -6,10 +6,8 @@ return [
             "name" => "laravel-kafka",
             "repository" => "mateusjunges/laravel-kafka",
             "branches" => [
-                "v1.3.x" => "v1.3",
-                "v1.4.x" => "v1.4",
-                "v1.5.x" => "v1.5",
                 "v1.6.x" => "v1.6",
+                "v1.7.x" => "v1.7",
                 "master" => "v2"
             ],
             "category" => "Laravel"
