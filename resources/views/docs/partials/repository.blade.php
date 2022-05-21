d7af347b17a67aca325a3b35e7f8f4601d962e0f
mateusjunges <mateus@junges.dev>
2022-05-21 01:05:48 -0300

    wip

--- resources/views/docs/partials/repository.blade.php
+++ resources/views/docs/partials/repository.blade.php
@@ -1,10 +1,10 @@
-<div class="w-screen md:w-1/2 border-opacity-25 px-2 md:px-0 min-h-90px my-4 border-l-2 border-gray-light">
-    <div class="bg-white flex flex-col justify-between px-4">
+<div class="w-screen md:w-1/2 border-opacity-25 px-2 md:px-0 min-h-90px my-4 border-l-2 border-white">
+    <div class="bg-black flex flex-col justify-between px-4">
         <div class="mt-2">
-            <a href="{{ action([\App\Http\Controllers\Docs\DocsController::class, 'repository'], $repository->slug) }}" class="text-2xl font-bold text-gray-700 hover:underline">
+            <a href="{{ action([\App\Http\Controllers\Docs\DocsController::class, 'repository'], $repository->slug) }}" class="text-2xl font-bold text-white hover:underline">
                 mateusjunges/{{ $repository->slug }}
             </a>
-            <p class="mt-2 text-gray-600 px-2">
+            <p class="mt-2 text-white px-2">
                 {{ optional($repository->aliases->last())->slogan }}
             </p>
         </div>
@@ -12,7 +12,7 @@
             Available Versions:
             @foreach($repository->aliases as $alias)
                 <span>
-                    <a class="inline-flex items-center justify-center rounded-full w-6 h-6 bg-opacity-50 hover:bg-opacity-100 hover:text-white {{ $loop->first ? 'bg-black text-black font-bold' : 'bg-gray-lightest text-gray-dark'}}" href="{{action([\App\Http\Controllers\Docs\DocsController::class, 'repository'], [$repository->slug, $alias->slug])}}">
+                    <a class="inline-flex items-center justify-center rounded-full w-8 h-8 px-2 bg-opacity-50 hover:bg-opacity-100 hover:font-super-bold {{ $loop->first ? 'bg-white text-white hover:text-black font-bold' : 'bg-simple-black text-white hover:font-super-bold'}}" href="{{action([\App\Http\Controllers\Docs\DocsController::class, 'repository'], [$repository->slug, $alias->slug])}}">
                         {{ $alias->slug }}
                     </a>
                 </span>
@@ -21,8 +21,8 @@
         <div class="flex items-center justify-between mt-4">
             <div class="flex justify-between">
                 <a href="#" class="flex items-center">
-                    <svg width="18" height="18" viewBox="0 0 24 24" class="mr-2"><path fill="black" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
-                    <h1 class="font-bold text-gray-700 hover:underline"> View source code</h1>
+                    <svg width="18" height="18" viewBox="0 0 24 24" class="mr-2"><path fill="white" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+                    <h1 class="font-bold text-white hover:underline"> View source code</h1>
                 </a>
             </div>
             <div class="flex justify-between items-center">
--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,12 +1,12 @@
-<nav id="header" class="fixed w-full z-10 pin-t bg-white -mt-16 shadow">
+<nav id="header" class="fixed w-full z-10 pin-t bg-black text-white -mt-16 shadow">
     <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-4">
         <div class="pl-4 items-center">
-            <a class="text-black text-base no-underline hover:no-underline font-extrabold text-xl"  href="{{ route('home') }}">
+            <a class="text-white text-base no-underline hover:no-underline font-extrabold text-xl"  href="{{ route('home') }}">
                 JUNGES.DEV
             </a>
         </div>
         <div class="block lg:hidden pr-4">
-            <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-grey border-grey-dark hover:text-black hover:border-purple appearance-none focus:outline-none">
+            <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-grey border-grey-dark hover:text-white hover:border-purple appearance-none focus:outline-none">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <title>Menu</title>
                     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
@@ -16,8 +16,8 @@
         <div class="w-full lg:flex lg:content-center lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 z-20 justify-between" id="nav-content">
             {{ \Spatie\Menu\Laravel\Menu::navbar()
                 ->addClass('list-reset lg:flex justify-end items-center')
-                ->addItemClass('inline-block text-grey-dark no-underline hover:text-black hover:underline py-2 px-4')
-                ->setActiveClass('font-bold text-black mb:border-b-2 mb:border-black')
+                ->addItemClass('inline-block text-grey-dark no-underline hover:text-white hover:underline py-2 px-4')
+                ->setActiveClass('font-bold text-white mb:border-b-2 mb:border-black')
                 }}
         </div>
     </div>
