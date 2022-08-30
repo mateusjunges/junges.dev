8c1e8678ef4b126240029261d4f0642e702c24a0
Mateus Junges <mateus@junges.dev>
2022-08-30 19:48:44 -0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(ImportPackagistDownloadsCommand::class)->hourly();
+        $schedule->command(ImportDocsFromRepositoriesCommand::class)->everyMinute();
     }
 
     protected function commands()
