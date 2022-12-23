3ded26fb51ae80b662ad4ebf855bdf9d320cc692
Mateus Junges <mateus@junges.dev>
2022-12-23 13:11:25 -0300

    Add post routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\CommunityController;
 use App\Http\Controllers\Docs\DocsController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OpenSourceController;
+use App\Modules\Posts\Http\Controllers\OriginalsController;
+use App\Modules\Posts\Http\Controllers\ShowPostController;
 use Illuminate\Support\Facades\Route;
 
 Route::any('/', HomeController::class)->name('home');
@@ -18,3 +20,6 @@ Route::get('blog', [BlogController::class, 'index'])->name('blog.index');
 Route::get('documentation', [DocsController::class, 'index'])->name('docs');
 Route::get('documentation/{repository}/{alias?}', [DocsController::class, 'repository']);
 Route::get('documentation/{repository}/{alias}/{slug}', [DocsController::class, 'show'])->where('slug', '.*');
+
+Route::get('posts', OriginalsController::class)->name('posts.originals');
+Route::get('posts/{post:slug}', ShowPostController::class)->name('posts.show');
