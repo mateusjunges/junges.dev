de7b64203249b2b07cfddef17305baa4053dfa22
Mateus Junges <mateus@junges.dev>
2022-12-25 14:31:06 -0300

    Add filament stles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
     content: [
         './resources/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
         './app/*.php',
 
     ],
