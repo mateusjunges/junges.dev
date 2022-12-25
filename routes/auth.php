2d8f51ad9501d608d9e91a778bbf9592e576a0a7
Mateus Junges <mateus@junges.dev>
2022-12-25 13:05:33 -0300

    Do not use routes directory

--- routes/auth.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types=1);
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('login', fn () => 'Login')->name('login');
-Route::get('register', fn () => 'Register')->name('register');
