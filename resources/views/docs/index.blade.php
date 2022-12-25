8e5a8154161061307ae77506f05b0c0d36302339
Mateus Junges <mateus@junges.dev>
2022-12-25 11:49:01 -0300

    Remove dead code

--- resources/views/docs/index.blade.php
+++ resources/views/docs/index.blade.php
@@ -11,20 +11,6 @@
                 <a href="https://github.com/mateusjunges" class="underline font-bold">at github</a>.
             </p>
         </div>
-{{--        <div class="search mt-6">--}}
-{{--            <div class="container flex w-full">--}}
-{{--                <div class="flex border mx-auto w-full">--}}
-{{--                    <button class="flex items-center justify-center px-4">--}}
-{{--                        <svg class="w-6 h-6 text-gray-600" fill="currentColor" viewBox="0 0 24 24">--}}
-{{--                            <path--}}
-{{--                                d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z">--}}
-{{--                            </path>--}}
-{{--                        </svg>--}}
-{{--                    </button>--}}
-{{--                    <input type="text" class="px-4 py-2 w-80" placeholder="Looking for an specific repository?">--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
         <div class="mt-6">
             @foreach($repositories->groupBy('category') as $category => $repositories)
                 <div class="mb-24">
