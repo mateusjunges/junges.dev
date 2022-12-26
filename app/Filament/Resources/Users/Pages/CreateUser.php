e5545d1552422846697f34fdce7235b6d0edad03
Mateus Junges <mateus@junges.dev>
2022-12-26 11:40:10 -0300

    Allow to manage users

---
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace App\Filament\Resources\Users\Pages;
+
+use App\Filament\Resources\Users\UserResource;
+use Filament\Resources\Pages\CreateRecord;
+
+final class CreateUser extends CreateRecord
+{
+    protected static string $resource = UserResource::class;
+
+}
