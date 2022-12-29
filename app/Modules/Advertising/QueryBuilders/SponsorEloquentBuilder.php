151d531391e3dc917c9b0e6e63d49aa1a7ffefa8
Mateus Junges <mateus@junges.dev>
2022-12-28 23:25:11 -0300

    Add sponsor eloquent builder

---
+++ app/Modules/Advertising/QueryBuilders/SponsorEloquentBuilder.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Modules\Advertising\QueryBuilders;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @template TModelClass of \App\Modules\Advertising\Models\Sponsor
+ * @extends \Illuminate\Database\Eloquent\Builder<TModelClass>
+ */
+final class SponsorEloquentBuilder extends Builder
+{
+
+}
