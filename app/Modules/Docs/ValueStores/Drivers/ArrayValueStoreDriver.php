bec67a2db6f3b30bd2a9a8a09b0482da2559c488
Mateus Junges <mateus@junges.dev>
2022-12-29 23:31:03 -0300

    Add 2 implementations for ValueStoreDriver contract

---
+++ app/Modules/Docs/ValueStores/Drivers/ArrayValueStoreDriver.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace App\Modules\Docs\ValueStores\Drivers;
+
+use App\Modules\Docs\Contracts\ValueStoreDriver;
+
+final class ArrayValueStoreDriver implements ValueStoreDriver
+{
+    private static array $store = [];
+
+    public function getNames(): array
+    {
+        return self::$store['updatedRepositoryNames'] ?? [];
+    }
+
+    public function store(string $name): ValueStoreDriver
+    {
+        self::$store['updatedRepositoryNames'][] = $name;
+
+        return $this;
+    }
+
+    public function flush(): void
+    {
+        self::$store = [];
+    }
+}
---
+++ app/Modules/Docs/ValueStores/Drivers/FileValueStoreDriver.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\Modules\Docs\ValueStores\Drivers;
+
+use App\Modules\Docs\Contracts\ValueStoreDriver;
+use Illuminate\Support\Arr;
+use Spatie\Valuestore\Valuestore;
+
+final class FileValueStoreDriver implements ValueStoreDriver
+{
+    public function __construct(
+        private readonly Valuestore $store,
+    ) {}
+
+    public function getNames(): array
+	{
+        return Arr::wrap($this->store->get('updatedRepositoryNames', []));
+	}
+
+	public function store(string $name): ValueStoreDriver
+	{
+        $updatedRepositoryNames = $this->store->get('updatedRepositoryNames', []);
+
+        $updatedRepositoryNames[] = $name;
+
+        $this->store->put('updatedRepositoryNames', array_unique($updatedRepositoryNames));
+
+        return $this;
+    }
+
+	public function flush(): void
+	{
+        $this->store->flush();
+	}
+}
