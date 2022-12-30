94e010dd47cd21bf7352192a87d8484e3d2c8c7d
Mateus Junges <mateus@junges.dev>
2022-12-30 13:41:31 -0300

    Add link to admin panel

--- app/Providers/NavigationServiceProvider.php
+++ app/Providers/NavigationServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 ;
 
 use App\Modules\Auth\Http\Controllers\LoginController;
+use App\Modules\Auth\Models\User;
 use App\Modules\Home\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Menu\Laravel\Menu;
 
@@ -27,8 +29,16 @@ class NavigationServiceProvider extends ServiceProvider
                 ->route('links.index', 'Links')
                 ->setActiveFromRequest();
 
-            if (auth()->check()) {
+            $user = Auth::user();
+            if ($user) {
+                assert($user instanceof User);
+
+                if ($user->admin) {
+                    $menu->url('/admin', 'Admin Panel');
+                }
+
                 $menu->action([LoginController::class, 'logout'], 'Logout');
+
             } else {
                 $menu->action([LoginController::class, 'showLoginForm'], 'Login');
             }
