a6117ace84f4877ed77239f802b7ddefe19d490a
Mateus Junges <mateus@junges.dev>
2022-12-31 16:57:54 -0300

    wip

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         \App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         \App\Providers\TwitterServiceProvider::class,
         \App\Providers\ViewServiceProvider::class,
         \App\Providers\BladeComponentServiceProvider::class,
