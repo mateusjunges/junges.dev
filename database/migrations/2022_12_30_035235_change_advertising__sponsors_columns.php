eae03f75d91b0c144bf45fd619ce923b8ed9eacd
Mateus Junges <mateus@junges.dev>
2024-02-20 19:46:35 -0300

    Run tests on sqlite

--- database/migrations/2022_12_30_035235_change_advertising__sponsors_columns.php
+++ database/migrations/2022_12_30_035235_change_advertising__sponsors_columns.php
@@ -9,11 +9,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('advertising__sponsors', function (Blueprint $table) {
-            $table->after('name', function (Blueprint $table) {
-                $table->string('github_username')->nullable();
-                $table->string('github_avatar_url')->nullable();
-            });
-            $table->renameColumn('sponsor_tier', 'monthly_price_in_dollars');
         });
     }
 };
