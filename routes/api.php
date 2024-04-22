516a2dbb7d09d942f608c5a846a45d397de278f7
Mateus Junges <mateus@junges.dev>
2024-04-22 20:22:29 -0300

    Fix routes

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use App\Modules\Docs\Http\Controllers\Webhooks\HandleGithubStarWebhookController
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->group(static function () {
-    Route::post('api/webhooks/github', [HandleGithubRepositoryWebhookController::class, 'handle'])->name('api.docs.webhooks.github.repository');
-    Route::post('api/webhooks/github/repo-starred', [HandleGithubStarWebhookController::class, 'handle'])->name('api.docs.webhooks.github.repo-starred');
-    Route::post('api/webhooks/github/repo-forked', [HandleGithubRepoForkedWebhookController::class, 'handle'])->name('api.docs.webhooks.github.repo-forked');
+    Route::post('webhooks/github', [HandleGithubRepositoryWebhookController::class, 'handle'])->name('api.docs.webhooks.github.repository');
+    Route::post('webhooks/github/repo-starred', [HandleGithubStarWebhookController::class, 'handle'])->name('api.docs.webhooks.github.repo-starred');
+    Route::post('webhooks/github/repo-forked', [HandleGithubRepoForkedWebhookController::class, 'handle'])->name('api.docs.webhooks.github.repo-forked');
 
-    Route::post('api/advertising/webhooks/github/sponsors', [HandleSponsorshipWebhookController::class, 'handle'])->name('api.advertising.webhooks.github.sponsors');
+    Route::post('advertising/webhooks/github/sponsors', [HandleSponsorshipWebhookController::class, 'handle'])->name('api.advertising.webhooks.github.sponsors');
 });
