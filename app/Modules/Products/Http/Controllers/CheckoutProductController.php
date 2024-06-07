c4f6f391ee8cfc4a9ea18b5f16035a5c68dfcd33
Mateus Junges <mateus@junges.dev>
2024-06-06 22:10:38 -0300

    wip

--- app/Modules/Products/Http/Controllers/CheckoutProductController.php
+++ app/Modules/Products/Http/Controllers/CheckoutProductController.php
@@ -6,13 +6,12 @@ use App\Modules\Products\Models\Customer;
 use App\Modules\Products\Models\PairingSession;
 use App\Modules\Products\Models\Product;
 use App\Modules\Products\Requests\CheckoutProductRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Laravel\Cashier\Checkout;
 
 final class CheckoutProductController
 {
-    public function store(Request $request): Checkout
+    public function store(CheckoutProductRequest $request): Checkout
     {
         $customer = Customer::query()->firstOrCreate(
             attributes: ['email' => $request->input('email')],
@@ -21,16 +20,20 @@ final class CheckoutProductController
 
         assert($customer instanceof Customer);
 
-        $product = Product::query()->where('stripe_product_id', $request->input('stripe_product_id'))->first();
+        $product = Product::query()
+            ->where('stripe_product_id', $request->input('stripe_product_id'))
+            ->first();
+
         assert($product instanceof Product);
 
         $pairingSession = PairingSession::query()->create([
             'customer_id' => $customer->id,
             'product_id' => $product->id
         ]);
+
         assert($pairingSession instanceof PairingSession);
 
-        return $customer->checkout([$request->input('stripe_price_id') => 1], [
+        return $customer->checkout([$product->stripe_price_id => 1], [
             'success_url' => URL::temporarySignedRoute('checkout.success', now()->addMinutes(5), ['pairing_session' => $pairingSession->id]),
             'cancel_url' => URL::temporarySignedRoute('checkout.failed', now()->addMinutes(5), ['pairing_session' => $pairingSession->id]),
             'metadata' => [
