97277bc8710a44d89569cc5afb9cc191473a6bf2
Mateus Junges <mateus@junges.dev>
2024-11-16 01:42:09 -0300

    wip

--- config/docs.php
+++ config/docs.php
@@ -12,23 +12,6 @@ return [
             ],
             'category' => 'Laravel',
         ],
-        [
-            'name' => 'laravel-acl',
-            'repository' => 'mateusjunges/laravel-acl',
-            'branches' => [
-                'master' => 'v4',
-            ],
-            'category' => 'Laravel',
-        ],
-        [
-            'name' => 'trackable-jobs-for-laravel',
-            'repository' => 'mateusjunges/trackable-jobs-for-laravel',
-            'branches' => [
-                'v1.6.x' => 'v1.6',
-                'master' => 'v2',
-            ],
-            'category' => 'Laravel',
-        ],
     ],
 
     'valuestore' => [
